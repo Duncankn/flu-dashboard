f16a6e97247aa2fb34428337279fd8aa3d969a9b
duncankn <duncankn@gmail.com>
2025-02-18 16:23:34 +0800

    change it back to original

--- next.config.ts
+++ next.config.ts
@@ -2,11 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  output: 'export',
-  basePath: '/flu-dashboard',
-  images: {
-    unoptimized: true,
-  },
+
 };
 
 export default nextConfig;
