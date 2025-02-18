a9cf9b50bbf88dad9020a079a52377b558148b07
duncankn <duncankn@gmail.com>
2025-02-18 16:41:24 +0800

    update Adsense script

--- components/cookieConsentBanner.tsx
+++ components/cookieConsentBanner.tsx
@@ -21,7 +21,7 @@ const CookieConsentBanner = () => {
 
   const loadAdSenseScript = () => {
     const script = document.createElement('script');
-    script.src = `https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8470057760537876`;
+    script.src = "https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8470057760537876";
     script.async = true;
     script.crossOrigin = 'anonymous';
     document.body.appendChild(script);
