7311849fd2f8606e923a4530a21eeaf3b5db0b78
duncankn <duncankn@gmail.com>
2025-02-26 10:07:19 +0800

    remove 301 redirect

--- next.config.ts
+++ next.config.ts
@@ -3,12 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   async redirects() {
     return [
-      {
-        source: '/:path*',
-        has: [{ type: 'header', key: 'host', value: 'www.flu-dashboard.vercel.app' }],
-        destination: 'https://flu-dashboard.vercel.app/:path*',
-        permanent: true, // Or 308
-      },
+
     ]
   },
 };
